7c3ae3d90e9f39c44ef249a2676c8a4778d0190e
Adam Bezecny <adam.bezecny@gmail.com>
2022-04-30 10:58:13 +0200

    background - wip

--- examples/03_background.rs
+++ examples/03_background.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
+use std::boxed::Box;
 
 const SEGMENT_HEIGHT: f32 = 40.;
 const SCREEN_HEIGHT: f32 = 800.;
 const SCREEN_WIDTH: f32 = 480.;
+#[allow(dead_code)]
 const SEGMENT_OVERLAP_OFFSET: f32 = 5.;
 
 fn main() {
@@ -19,9 +21,40 @@ fn main() {
         .run();
 }
 
+trait Row {
+    fn next(&self) -> Box<dyn Row>;
+    fn get_index(&self) -> i8;
+    fn get_img_base(&self) -> String;
+    fn get_img_name(&self) -> String {
+        format!("images/{}{}.png", self.get_img_base(), self.get_index())
+    }
+}
 
-trait RowSegment {
-    fn next(&self) -> Self;
+fn draw_n_rows(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    row: Box<dyn Row>,
+    n: i8,
+    offset_from_bottom: f32,
+) {
+    let mut row_to_draw = row;
+
+    for i in 0..n {
+        if i > 0 {
+            row_to_draw = row_to_draw.next();
+        }
+
+        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT * (i as f32 + 1.) - SEGMENT_HEIGHT / 2.
+            + offset_from_bottom;
+
+        println!("drawing {}", row_to_draw.get_img_name());
+
+        commands.spawn_bundle(SpriteBundle {
+            texture: asset_server.load(&row_to_draw.get_img_name()),
+            transform: Transform::from_xyz(0., y, 0.),
+            ..default()
+        });
+    }
 }
 
 /// rail
@@ -31,15 +64,51 @@ struct RailRow {
 
 impl RailRow {
     fn new_rail_row(index: i8) -> Self {
-        RailRow {
-            index,
-        }
+        RailRow { index }
     }
 }
 
-impl RowSegment for RailRow {
-    fn next(&self) -> Self {
-        RailRow::new_rail_row(1)
+impl Row for RailRow {
+    fn next(&self) -> Box<dyn Row> {
+        if self.index < 3 {
+            Box::new(RailRow::new_rail_row(self.index + 1))
+        } else {
+            Box::new(WaterRow::new_water_row(0))
+        }
+    }
+
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "rail".to_string()
+    }
+}
+
+/// water
+struct WaterRow {
+    index: i8,
+}
+
+impl WaterRow {
+    fn new_water_row(index: i8) -> Self {
+        WaterRow { index }
+    }
+}
+
+impl Row for WaterRow {
+    fn next(&self) -> Box<dyn Row> {
+        if self.index < 7 {
+            Box::new(WaterRow::new_water_row(self.index + 1))
+        } else {
+            Box::new(PavementRow::new_pavement_row(0))
+        }
+    }
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "water".to_string()
     }
 }
 
@@ -50,18 +119,112 @@ struct PavementRow {
 
 impl PavementRow {
     fn new_pavement_row(index: i8) -> Self {
-        PavementRow {
-            index,
+        PavementRow { index }
+    }
+}
+
+impl Row for PavementRow {
+    fn next(&self) -> Box<dyn Row> {
+        if self.index < 2 {
+            Box::new(PavementRow::new_pavement_row(self.index + 1))
+        } else {
+            Box::new(RoadRow::new_road_row(0))
         }
     }
-}
-
-impl RowSegment for PavementRow {
-    fn next(&self) -> Self {
-        PavementRow::new_pavement_row(1)
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "side".to_string()
     }
 }
 
+/// road
+struct RoadRow {
+    index: i8,
+}
+
+impl RoadRow {
+    fn new_road_row(index: i8) -> Self {
+        RoadRow { index }
+    }
+}
+
+impl Row for RoadRow {
+    fn next(&self) -> Box<dyn Row> {
+        if self.index < 2 {
+            Box::new(RoadRow::new_road_row(self.index + 1))
+        } else {
+            Box::new(WaterRow::new_water_row(0))
+        }
+    }
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "road".to_string()
+    }
+}
+
+/// dirt
+struct DirtRow {
+    index: i8,
+}
+
+impl DirtRow {
+    fn new_dirt_row(index: i8) -> Self {
+        DirtRow { index }
+    }
+}
+
+impl Row for DirtRow {
+    fn next(&self) -> Box<dyn Row> {
+        match self.index {
+            1..=5 => Box::new(DirtRow::new_dirt_row(self.index + 8)),
+            6 => Box::new(DirtRow::new_dirt_row(7)),
+            7 => Box::new(DirtRow::new_dirt_row(15)),
+            8..=14 => Box::new(DirtRow::new_dirt_row(self.index + 1)),
+            _ => Box::new(WaterRow::new_water_row(0)),
+        }
+    }
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "dirt".to_string()
+    }
+}
+
+///grass
+struct GrassRow {
+    index: i8,
+}
+
+impl GrassRow {
+    fn new_grass_row(index: i8) -> Self {
+        GrassRow { index }
+    }
+}
+
+impl Row for GrassRow {
+    fn next(&self) -> Box<dyn Row> {
+        match self.index {
+            1..=5 => Box::new(GrassRow::new_grass_row(self.index + 8)),
+            6 => Box::new(GrassRow::new_grass_row(7)),
+            7 => Box::new(GrassRow::new_grass_row(15)),
+            8..=14 => Box::new(GrassRow::new_grass_row(self.index + 1)),
+            _ => Box::new(WaterRow::new_water_row(0)),
+        }
+    }
+    fn get_index(&self) -> i8 {
+        self.index
+    }
+    fn get_img_base(&self) -> String {
+        "grass".to_string()
+    }
+}
+
+#[allow(dead_code)]
 fn add_segments(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
@@ -81,45 +244,33 @@ fn add_segments(
     }
 }
 
-fn add_segment(
-    commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
-    segment_index: i8,
-    image_base: &str,
-    offset_from_bottom: f32,
-) {
-        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT - SEGMENT_HEIGHT / 2.
-            + offset_from_bottom;
-        println!("y=={}", y);
-        commands.spawn_bundle(SpriteBundle {
-            texture: asset_server.load(&format!("images/{}{}.png", image_base, segment_index)),
-            transform: Transform::from_xyz(0., y, 0.),
-            ..default()
-        });
-
-}
-
 // https://users.rust-lang.org/t/solved-placement-of-mut-in-function-parameters/19891
+#[allow(dead_code)]
 fn add_rail(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 4, "rail", offset_from_bottom);
 }
 
+#[allow(dead_code)]
 fn add_road(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 6, "road", offset_from_bottom);
 }
 
+#[allow(dead_code)]
 fn add_side(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 3, "side", offset_from_bottom);
 }
 
+#[allow(dead_code)]
 fn add_water(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 8, "water", offset_from_bottom);
 }
 
+#[allow(dead_code)]
 fn add_grass(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 15, "grass", offset_from_bottom);
 }
 
+#[allow(dead_code)]
 fn add_dirt(commands: &mut Commands, asset_server: &Res<AssetServer>, offset_from_bottom: f32) {
     add_segments(commands, asset_server, 15, "dirt", offset_from_bottom);
 }
@@ -132,16 +283,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     add_water(&mut commands, &asset_server, 160. - SEGMENT_OVERLAP_OFFSET);
     add_side(&mut commands, &asset_server, 160. + 320.- SEGMENT_OVERLAP_OFFSET);*/
 
-
-    // just the road
-    // add_road(&mut commands, &asset_server, 0.);
-
-    // some grass
-    add_segment(&mut commands, &asset_server, 15, "grass", SEGMENT_OVERLAP_OFFSET);
-
-    //add_dirt(&mut commands, &asset_server, 0.);
-
-    //
-    //
-
+    draw_n_rows(
+        &mut commands,
+        &asset_server,
+        Box::new(RailRow::new_rail_row(0)),
+        4 + 8 + 3 + 4,
+        0.,
+    );
 }
