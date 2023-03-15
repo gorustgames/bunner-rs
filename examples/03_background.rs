135ef7b23bc3c92ce77012e417b2c8876623feb2
Adam Bezecny <adam.bezecny@gmail.com>
2023-03-15 22:10:44 +0100

    sprite anchoring changed from Center to BottomLeft

--- examples/03_background.rs
+++ examples/03_background.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::sprite::Anchor;
 use std::boxed::Box;
 
 const SEGMENT_HEIGHT: f32 = 40.;
@@ -44,14 +45,18 @@ fn draw_n_rows(
             row_to_draw = row_to_draw.next();
         }
 
-        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT * (i as f32 + 1.) - SEGMENT_HEIGHT / 2.
-            + offset_from_bottom;
+        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT * (i as f32) + offset_from_bottom;
+        let x = -1. * (SCREEN_WIDTH / 2.);
 
         println!("drawing {}", row_to_draw.get_img_name());
 
         commands.spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                anchor: Anchor::BottomLeft,
+                ..default()
+            },
             texture: asset_server.load(&row_to_draw.get_img_name()),
-            transform: Transform::from_xyz(0., y, 0.),
+            transform: Transform::from_xyz(x, y, 0.),
             ..default()
         });
     }
@@ -233,12 +238,16 @@ fn add_segments(
     offset_from_bottom: f32,
 ) {
     for i in 0..count {
-        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT * (i as f32 + 1.) - SEGMENT_HEIGHT / 2.
-            + offset_from_bottom;
+        let y = -1. * (SCREEN_HEIGHT / 2.) + SEGMENT_HEIGHT * (i as f32) + offset_from_bottom;
+        let x = -1. * (SCREEN_WIDTH / 2.);
         println!("y=={}", y);
         commands.spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                anchor: Anchor::BottomLeft,
+                ..default()
+            },
             texture: asset_server.load(&format!("images/{}{}.png", image_base, i)),
-            transform: Transform::from_xyz(0., y, 0.), // see https://bevy-cheatbook.github.io/features/coords.html
+            transform: Transform::from_xyz(x, y, 0.), // see https://bevy-cheatbook.github.io/features/coords.html
             ..default()
         });
     }
@@ -279,15 +288,21 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // rail+water+pavement
-    /*add_rail(&mut commands, &asset_server, 0.);
-    add_water(&mut commands, &asset_server, 160. - SEGMENT_OVERLAP_OFFSET);
-    add_side(&mut commands, &asset_server, 160. + 320.- SEGMENT_OVERLAP_OFFSET);*/
+    /*add_rail(&mut commands, &asset_server, 0.); // 4 rails x 40 px
+    add_water(&mut commands, &asset_server, 160.); // 8 waters x 40 px
+    add_side(&mut commands, &asset_server, 160. + 320.); */
+
+    let rail_count = 4;
+    let water_count = 8;
+    let sidewalk_count = 3;
+    let road_count = 3;
+    let water_count_2 = 2; // we have place just for 2 blocks. together we have 20 x 40 px = 800 px which is screen height
 
     draw_n_rows(
         &mut commands,
         &asset_server,
         Box::new(RailRow::new_rail_row(0)),
-        4 + 8 + 3 + 4,
+        rail_count + water_count + sidewalk_count + road_count + water_count_2,
         0.,
-    );
+    ); /**/
 }
