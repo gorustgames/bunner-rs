411e6f32a7cd9baa719de8615c1e592145b20cc5
Adam Bezecny <adam.bezecny@sporticon.cz>
2023-03-29 10:07:58 +0200

    code dedup

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,15 @@ pub fn get_random_float() -> f64 {
     rng.gen::<f64>()
 }
 
-/// returns a random value from interval <from, to>
-pub fn get_random_i8(from: i8, to: i8) -> i8 {
-    let mut rng = rand::thread_rng();
-    // https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html#generate-random-numbers-within-a-range
-    rng.gen_range(from..to + 1)
-}
-
 /// same as get_random_i8. for now keep it simple, no generics
+/// https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html#generate-random-numbers-within-a-range
 pub fn get_random_i32(from: i32, to: i32) -> i32 {
     let mut rng = rand::thread_rng();
     rng.gen_range(from..to + 1)
+}
+
+pub fn get_random_i8(from: i8, to: i8) -> i8 {
+    get_random_i32(from as i32, to as i32) as i8
 }
 
 pub fn is_even_number<T>(num: T) -> bool
