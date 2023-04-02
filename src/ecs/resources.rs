a78ca24b90cf7d2b61fb4f6f30edea8934a874a8
Adam Bezecny <adam.bezecny@gmail.com>
2023-04-02 22:05:03 +0200

    Background rows resource implemented

--- src/ecs/resources.rs
+++ src/ecs/resources.rs
@@ -1,3 +1,19 @@
-/// placeholder for future custom resources
-#[allow(unused_imports)]
-use bevy::prelude::*;
+use crate::ecs::components::background_row::BackgroundRow;
+use sliding_window::typenum::consts::*;
+use sliding_window::*;
+
+pub struct BackgroundRows {
+    rows: SlidingWindow<BackgroundRow, U20>,
+}
+
+impl BackgroundRows {
+    pub fn new() -> Self {
+        BackgroundRows {
+            rows: SlidingWindow::new(),
+        }
+    }
+
+    pub fn add_row(&mut self, bg_row: BackgroundRow) {
+        self.rows.insert(bg_row);
+    }
+}
