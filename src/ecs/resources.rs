2341d843b68ad69c0de06f5c6d4630c9d7d22647
Adam Bezecny <adam.bezecny@gmail.com>
2023-04-04 22:32:54 +0200

    BackgroundRows::last_row added

--- src/ecs/resources.rs
+++ src/ecs/resources.rs
@@ -16,4 +16,32 @@ impl BackgroundRows {
     pub fn add_row(&mut self, row: Box<dyn Row>) {
         self.rows.insert(row);
     }
+
+    pub fn last_row(&self) -> Option<Box<dyn Row>> {
+        return if self.rows.count() == 0 {
+            None
+        } else {
+            Some(self.rows[self.rows.count() - 1].clone_row())
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ecs::components::background_row::GrassRow;
+
+    #[test]
+    fn test_last_row() {
+        let mut bgr = BackgroundRows::new();
+        assert_eq!(bgr.last_row().is_none(), true);
+        let mut grass_row = GrassRow::new_grass_row(0);
+        let mask: [bool; 12] = [
+            true, true, true, false, false, false, true, true, true, false, true, false,
+        ];
+        grass_row.set_row_mask(mask);
+        bgr.add_row(Box::new(grass_row));
+        let previous_row = bgr.last_row();
+        assert_eq!(previous_row.unwrap().get_row_mask(), Some(mask));
+    }
 }
